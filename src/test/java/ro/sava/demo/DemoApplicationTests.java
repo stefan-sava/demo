efe512f4ce23cc776c8dfc4debd71ca7389f2d1a
Stefan Sava <stefan@sava.ro>
2021-01-13 21:03:56 +0200

    primul commit

---
+++ src/test/java/ro/sava/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.sava.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
